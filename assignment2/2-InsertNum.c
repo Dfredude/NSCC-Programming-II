460b7c98cb497e4d0f4df7c635159c299332b2c3
Alfredo Dominguez Lucio <thecatura.ad@gmail.com>
2023-02-08 12:25:00 -0400

    finished assign 2.2

--- assignment2/2-InsertNum.c
+++ assignment2/2-InsertNum.c
@@ -7,19 +7,44 @@ Rather than simply overwriting the value at this position, your program will shi
 #include<stdio.h>
 
 void insertNum(int* nums, int num, int index);
+void printArrayOf10Ints(int* nums);
+
 
 int main(int argc, char const *argv[])
 {
-    int nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0};
+    int nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int num, index;
+    puts("Intial array of 10 numbers: ");
+    printArrayOf10Ints(nums);
     printf("Please enter a value to be inserted into the array: ");
-    scanf("%d", num);
-    printf("Please enter the position to insert the value at in the array: ");
-    scanf(" %d", index);
+    scanf(" %d", &num);
+    printf("Please enter the position to insert the value at in the array (0-9): ");
+    scanf(" %d", &index);
+    char ch;
+    while ( ( (ch = getchar()) != '\n' && ch != EOF)); // Flushing buffer
     insertNum(nums, num, index);
+    printf("Succesfully inserted \"%d\" into the array.\n", num);
+    printArrayOf10Ints(nums);
     return 0;
 }
 
 void insertNum(int* nums, int num, int index){
-    
+    // Shifting values
+    index++;
+    int prev = nums[index-1]; // value to shift right
+    int curr;
+    for (int i = index; i < 10; i++){
+        curr = nums[i]; // storing current value
+        nums[i] = prev;
+        prev = curr;
+    }
+    nums[--index] = num;
+}
+
+void printArrayOf10Ints(int* nums){
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", nums[i]);
+    }
+    puts("");    
 }
---
+++ assignment2/3-Student.c
@@ -0,0 +1,9 @@
+/* 3. 
+Create a program where you define a ‘Student’ structure to represent a 
+- student’s name
+- id 
+- grades for C, SAAD, OOP, COMM, APPD, JS. 
+Declare an array of 5 ‘Students’ and initialize them within your program.
+Loop through the array and display the student’s information as well as their average for the 6 courses.
+
+*/
